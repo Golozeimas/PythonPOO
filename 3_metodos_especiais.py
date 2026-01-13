62a61cc900db3835c9ab4752c08710e6d82802e1
Matheus+ <joaosobramatheus@gmail.com>
2026-01-13 09:39:13 -0300

    <study> Aprendizado sobre métodos de instância e variáveis de classe

--- 3_metodos_especiais.py
+++ 3_metodos_especiais.py
@@ -3,6 +3,7 @@ class Game:
     yearLaunch = 0
     note = 0.0
     multiplayer = False
+    total_games = 0
 
     # substitui o print(obj), podendo printar direto do objeto, sem precisar de acesso diret+o
     def __str__(self):
@@ -13,9 +14,37 @@ class Game:
         self.gameName = gameName
         self.yearLaunch = yearLaunch
         self.multiplayer = multiplayer
+        Game.total_games += 1
         self.note = note
+        self.notas = 0.0
+        self.avaliacao = 0
+
+    def substitui_os_dados(self):
+        self.gameName = input("Coloque o nome do jogo:\n")
+        self.note = float(input("Coloque a nota do jogo:\n"))
+        self.yearLaunch = int(input("Coloque o ano de lançamento do jogo:\n"))
+        print(f"- nome do jogo: {self.gameName},\n- nota do jogo: {self.note}\n e ano de lançamento: {self.yearLaunch}")    
+
+    def nota_do_jogo(self, note):
+        self.notas += note
+        self.avaliacao += 1
+    
+    def media_das_notas(self):
+        print(f"Nome do jogo: {self.gameName}, sua média de notas:{self.notas / self.avaliacao:.2f}")
 
 # criando um objeto depois desse métodos especiais
+"""
 game1 = Game("Demons Souls Remake", 2020, 10, True)
-
 print(game1)
+"""
+game1 = Game("Demons Souls Remake", 2020, 10, True)
+# notas e média
+game1.nota_do_jogo(6)
+game1.nota_do_jogo(7)
+game1.nota_do_jogo(10)
+game1.media_das_notas()
+
+game2 = Game("Dark souls", 2013, 10, True)
+
+# usando variável de instância para contar o número de jogos
+print(f"Números total de jogos: {Game.total_games}")
